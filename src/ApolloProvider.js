fae24bbdebc995f880c628c32c3c99ae2c05991f
atlas25git <atlas.sarthak@gmail.com>
2021-07-16 14:32:35 +0530

    Final

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'https://rocky-fjord-64833.herokuapp.com/'
 });
 
 const authLink = setContext(() => {
